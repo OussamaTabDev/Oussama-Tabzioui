9887f52abfd835336c7a5da2c8ef54c4042de872
Oussama Tabzioui <OussamaTabzioui09@gmail.com>
2025-07-25 23:24:23 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base:"oussama-tabzioui",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
